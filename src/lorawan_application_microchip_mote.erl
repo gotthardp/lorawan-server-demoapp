6de04080ed7a819963ce0a7eead2f58ad0d799ce
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2017-12-27 23:09:50 +0100

    API changed

--- src/lorawan_application_microchip_mote.erl
+++ src/lorawan_application_microchip_mote.erl
@@ -9,7 +9,7 @@
 -module(lorawan_application_microchip_mote).
 -behaviour(lorawan_application).
 
--export([init/1, handle_join/3, handle_uplink/4, handle_rxq/4]).
+-export([init/1, handle_join/3, handle_uplink/4, handle_rxq/5]).
 
 -include_lib("lorawan_server/include/lorawan.hrl").
 -include_lib("lorawan_server/include/lorawan_db.hrl").
@@ -29,7 +29,7 @@ handle_uplink(_Context, _RxQ, _LastAcked, _Frame) ->
 
 % the data structure is explained in
 % Lora_Legacy_Mote_Firmware/Includes/Board/MOTEapp.c:520
-handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways,
+handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways, _WillReply,
         #frame{data= <<Light:5/binary, Temp:3/binary>>}, []) ->
     lager:debug("PUSH_DATA ~w ~p ~p", [DevAddr, Light, Temp]),
     % display actual time
@@ -37,7 +37,7 @@ handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways,
     Time = lists:flatten(io_lib:format('~2..0b:~2..0b:~2..0b', [H, M, S])),
     {send, #txdata{port=2, data=list_to_binary(Time)}};
 
-handle_rxq(_Context, _Gateways, Frame, _State) ->
+handle_rxq(_Context, _Gateways, _WillReply, Frame, _State) ->
     {error, {unexpected_data, Frame}}.
 
 % end of file
